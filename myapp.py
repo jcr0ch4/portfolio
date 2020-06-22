d0530e9de1bceff5d667be4eb29ebf06b7671e66
jcr0h4 <jcr0ch4@gmail.com>
2020-06-21 23:19:14 -0300

    Corrigindo o layout

--- myapp.py
+++ myapp.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/home")
-def home():
-    return render_template('index.html')
+#@app.route("/home")
+#def home():
+#    return render_template('home.html')
 
 
 if __name__ == '__main__':
